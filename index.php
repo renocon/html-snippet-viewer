036d9d1328c6d355da4d38a853f2a2f4135ec743
ICE-WOLF <david.dexter.charles@gmail.com>
2016-01-30 18:16:15 -0400

    Final Update made

--- index.php
+++ index.php
@@ -1,34 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>For Dessi LoL</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-	<script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
-	<script src="snip.js"></script>
+    <meta charset="UTF-8">
+    <title>For Dessi LoL</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
+    <script src="snip.js"></script>
+    <link rel="stylesheet" href="snip.css">
 </head>
 <body>
 
-    <div class="container-fluid">
+<div class="container-fluid">
 
-        <div class="col-md-6">
-            <div>
-                <span >Paste HTML code here</span><span></span>
-                <span><button id='parsebutton'  style='background-color:#b19cd9;'>View Text</button></span>
-            </div>
-
-            <textarea rows="400" cols="90"  id='intext'></textarea>
-
+    <div class="col-md-6">
+        <div >
+            <span >Paste HTML code here</span><span>  </span>
+            <span><button id='parsebutton'  style='background-color:#b19cd9;'>See Result »</button></span>
         </div>
 
+        <textarea   id='intext'></textarea>
 
-        <div class="col-md-6">
-
-
-                <div class="panel-body" id='outtext'>A Basic Panel</div>
-
-        </div>
     </div>
+
+
+    <div class="col-md-6">
+
+
+        <div class="panel-body" id='outtext'>Results</div>
+
+    </div>
+</div>
 
 
 </body>
